c8b7aef7dcc56634ac3c0788bc932553228b642e
LAPTOP-BQT045FE\Dulger <a@b.com>
2018-02-28 13:26:26 +0300

    Test islemleri yapildi

---
+++ SeralizersTests1/ReadWriteCsvFileTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _deneme1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _deneme1.Tests
+{
+    
+    [TestClass()]
+    public class ReadWriteCsvFileTests
+    {
+        [TestMethod()]
+        public void ReadFileTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void WriteFileTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ SeralizersTests1/ReadWriteJsonFileTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _deneme1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _deneme1.Tests
+{
+    [TestClass()]
+    public class ReadWriteJsonFileTests
+    {
+        [TestMethod()]
+        public void ReadFileTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void WriteFileTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
